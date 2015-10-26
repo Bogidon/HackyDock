d2adec98f88860611ae94e366601db369686a6e0
Bogdan Vitoc <bogdanvitoc@gmail.com>
2015-10-26 17:41:44 -0400

    Initial Commit

---
+++ HackyDock/HackyDock.h
@@ -0,0 +1,13 @@
+//
+//  HackyDock.h
+//  HackyDock
+//
+//  Created by Bogdan Vitoc on 10/26/15.
+//  Copyright © 2015 Bogdan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HackyDock : NSObject
+
+@end
---
+++ HackyDock/HackyDock.m
@@ -0,0 +1,13 @@
+//
+//  HackyDock.m
+//  HackyDock
+//
+//  Created by Bogdan Vitoc on 10/26/15.
+//  Copyright © 2015 Bogdan. All rights reserved.
+//
+
+#import "HackyDock.h"
+
+@implementation HackyDock
+
+@end
